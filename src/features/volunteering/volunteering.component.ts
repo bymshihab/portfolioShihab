28ecb2dd3a1d37ef20b0a8549495d8a05ce9418b
bymashihab <khaledshihab221@gmail.com>
2025-01-08 22:14:40 +0600

    add initial components and setup for Tailwind CSS

---
+++ src/features/volunteering/volunteering.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-volunteering',
+  standalone: true,
+  imports: [],
+  templateUrl: './volunteering.component.html',
+  styleUrl: './volunteering.component.css'
+})
+export class VolunteeringComponent {
+
+}
